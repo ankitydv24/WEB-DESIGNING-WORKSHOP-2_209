00748e2645ae05583bb30063c1d14e10caf82740
ANKIT YADAV <ankityadav74609@gmail.com>
2026-04-09 01:16:05 +0530

    done

---
+++ JAVA SCRIPT/JS fundamentals/app.js
@@ -0,0 +1,58 @@
+
+//var name="ANKIT";
+//var age=20;
+//var isEmployed=true;
+//console.log( name);
+//console.log( age);
+
+
+
+//let name="ANKIT";
+//let age=20;
+//let isEmployed=true;
+//console.log(name);
+//console.log(age);
+
+
+
+//if(true)
+///{
+    
+   // var age=30;
+
+//}
+//console.log(age);
+
+
+
+//if(true)
+//{
+    
+    //let age=30;
+
+//}
+//console.log(age);
+
+
+
+//var name="ANKIT";
+//var age=20;
+//var isEmployed=true;
+///console.log(typeof name);
+//console.log(typeof age);
+
+
+
+//let name="ankit";
+//let age=20;
+//console.log("my name is "+name+" and my age is "+age);
+//console.log(`my name is ${name} and my age is ${age}`);
+
+
+
+//let data =null;
+//console.log(data);
+
+
+
+
